b504e1982f474442ad5ec581a257ce465a65196c
sachin-sankar <mail.sachinsankar@gmail.com>
2024-10-06 11:30:56 +0530

    changed keybinding and remove formatting for maskdown

--- nvim/lua/sachin/plugins/formatting.lua
+++ nvim/lua/sachin/plugins/formatting.lua
@@ -15,7 +15,6 @@ return {
 				html = { "prettier" },
 				json = { "prettier" },
 				yaml = { "prettier" },
-				markdown = { "prettier" },
 				graphql = { "prettier" },
 				liquid = { "prettier" },
 				lua = { "stylua" },
@@ -28,7 +27,7 @@ return {
 			},
 		})
 
-		vim.keymap.set({ "n", "v" }, "<leader>ff", function()
+		vim.keymap.set({ "n", "v" }, "<leader>fp", function()
 			conform.format({
 				lsp_fallback = true,
 				async = false,

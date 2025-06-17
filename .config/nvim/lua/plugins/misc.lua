23f73af92ad3871df49de25de13d17e01b34f8c7
sachin-sankar <mail.sachinsankar@gmail.com>
2025-06-17 16:21:28 +0530

    sync

--- .config/nvim/lua/plugins/misc.lua
+++ .config/nvim/lua/plugins/misc.lua
@@ -5,4 +5,10 @@ return {
     lazy = true,
     version = false, -- last release is way too old
   },
+  {
+    "zeioth/garbage-day.nvim",
+    dependencies = "neovim/nvim-lspconfig",
+    event = "VeryLazy",
+    opts = {},
+  },
 }
